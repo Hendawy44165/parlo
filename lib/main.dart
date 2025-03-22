95bd6f91bec6993c9ceef132a9c44df9db76bdd1
Hendawy44165 <amr.hendawy44165@gmail.com>
2025-03-22 02:49:29 +0200

    firebase setup is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:parlo/app.dart';
 import 'package:parlo/core/themes/color.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:parlo/firebase_options.dart';
 
-void main() {
+void main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await dotenv.load();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarColor: ColorsManager.white,
